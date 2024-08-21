72cfa977e3eba12d950d354ada86c154bed5f012
Iliess <m.iliess.abdelmadjid@gmail.com>
2024-08-21 11:02:08 +0200

    ajout du formualaire post/put

--- resources/views/restaurant/create.blade.php
+++ resources/views/restaurant/create.blade.php
@@ -3,16 +3,11 @@
 @endphp
 
 <h1>Create</h1>
-{{$id}}
 
 <form method="{{ isset($id) ? 'PUT' : 'POST' }}" action="{{ url('restaurants/create') }}">
     @csrf
-    {{print_r($id);}}
-    <!-- @if(isset($id))
-        @method('PUT')
-    @endif -->
-
-    <!-- Other form fields go here (if needed) -->
+    <!-- cette partie me permet de verifier que je suis dans le bon formulaire POST et non PUT ou inverssement  -->
+    {{isset($id)? $id : "pas d'id donc formulaire de creation" ;}}
 
     <button type="submit">Submit</button>
 </form>
